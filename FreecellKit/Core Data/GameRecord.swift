f3c4f8598d71079032e84497c69410ece0820be2
Jason Ji <uberjason@gmail.com>
2020-02-16 21:48:42 -0500

    Create xcdatamodel with GameRecord object containing result, moves, time. Add app group for Freecell iOS target to store the Core Data store. Add CoreDataStackManager as a Swift Package dependency.

---
+++ FreecellKit/Core Data/GameRecord.swift
@@ -0,0 +1,50 @@
+//
+//  GameRecord.swift
+//  FreecellKit
+//
+//  Created by Jason Ji on 2/15/20.
+//  Copyright © 2020 Jason Ji. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public enum GameResult: String {
+    case win, loss
+}
+
+public protocol GameRecord {
+    var result: GameResult { get set }
+    var moves: Int { get set }
+    var time: TimeInterval { get set }
+}
+
+public class CDGameRecord: NSManagedObject, GameRecord {
+    public var result: GameResult {
+        get {
+            return GameResult(rawValue: resultCD!)!
+        }
+        set {
+            resultCD = newValue.rawValue
+        }
+    }
+    
+    public var moves: Int {
+        get {
+            return Int(movesCD)
+        }
+        set {
+            movesCD = Int64(newValue)
+        }
+    }
+    
+    public var time: TimeInterval {
+        get {
+            return timeCD
+        }
+        set {
+            timeCD = newValue
+        }
+    }
+    
+}
