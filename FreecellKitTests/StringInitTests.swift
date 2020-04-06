f81c662ccdce7379c34f96c445c503cde58cb8b7
Jason Ji <uberjason@gmail.com>
2020-04-06 09:50:25 -0400

    Add unit tests for Foundation and Column

--- FreecellKitTests/StringInitTests.swift
+++ FreecellKitTests/StringInitTests.swift
@@ -46,17 +46,44 @@ class StringInitTests: XCTestCase {
         XCTAssertNil(nilFreecell)
     }
     
-    func testFoundationStringInit() {
-        // case: 0 cards - fail
-        // case: 1 card as Ace - init with card
-        // case: 1 card not as Ace: - fail
-        // case: 2 cards Ace+2 - init with sequence
-        // case: 2 cards Ace+2-different suit - fail
-        // case: 5 cards Ace-5 - init with sequence
+    func testFoundationStringInit() throws {
+        // case 0: 0 cards - fail
+        // case 1: 1 card as Ace - init with card
+        // case 2: 1 card not as Ace: - fail
+        // case 3: 2 cards Ace+2 - init with sequence
+        // case 4: 2 cards Ace+2-different suit - fail
+        // case 5: 5 cards Ace-5 - init with sequence
+        
+        let case0 = Foundation(text: "")
+        XCTAssertNil(case0)
+        
+        let case1 = try XCTUnwrap(Foundation(text: "♠️A"))
+        XCTAssert(case1.suit == .spades)
+        XCTAssert(case1.topItem == Card.ace.ofSpades)
+        
+        let case2 = Foundation(text: "♠️4")
+        XCTAssertNil(case2)
+        
+        let case3 = try XCTUnwrap(Foundation(text: "♠️A, ♠️2"))
+        XCTAssert(case3.suit == .spades)
+        XCTAssert(case3.topItem == Card.two.ofSpades)
+        
+        let case4 = Foundation(text: "♠️A, ❤️2")
+        XCTAssertNil(case4)
+        
+        let case5 = try XCTUnwrap(Foundation(text: "♠️A, ♠️2, ♠️3, ♠️4, ♠️5"))
+        XCTAssert(case5.suit == .spades)
+        XCTAssert(case5.topItem == Card.five.ofSpades)
     }
     
-    func testColumnStringInit() {
+    func testColumnStringInit() throws {
         // case: 0 cards - init empty
         // case: 5 cards: init with cards
+        
+        let emptyColumn = try XCTUnwrap(Column(text: ""))
+        XCTAssertEqual(emptyColumn.stack.count, 0)
+        
+        let filledColumn = try XCTUnwrap(Column(text: "♠️3, ❤️4, ♦️K, ♣️Q"))
+        XCTAssertEqual(filledColumn.stack, [Card.three.ofSpades, Card.four.ofHearts, Card.king.ofDiamonds, Card.queen.ofClubs])
     }
 }
